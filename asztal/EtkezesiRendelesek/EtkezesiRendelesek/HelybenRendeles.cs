56c8e48c612a143a58ed8c1dcfc90f2fa66a16ed
HajduBalazs101 <hajdub@kkszki.hu>
2026-09-22 09:59:01 +0200

    valami

---
+++ asztal/EtkezesiRendelesek/EtkezesiRendelesek/HelybenRendeles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtkezesiRendelesek 
+{
+    public class HelybenRendeles : Rendeles
+    {
+        private int asztalSzam;
+        public HelybenRendeles(int rendelesId, List<string> etelek, int ar, int asztalSzam) : base(rendelesId, etelek, ar)
+        {
+            this.asztalSzam = asztalSzam;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Asztal Szám: {asztalSzam}";
+        }   
+        
+    }
+}
